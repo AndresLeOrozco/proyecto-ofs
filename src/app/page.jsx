b58f81f93090950a284d518cc189bdf18f41976c
eduardoojeda <peleojeda@gmail.com>
2023-09-10 11:17:36 -0600

    Modfying about button and delete its own page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,20 @@
 import Image from 'next/image'
-import TextArea from '@/components/TextArea'
 
 
-export default function Home() {
- 
 
+const Home =  () => {
+  // const data = getAbout()
+  
 }
+
+// getAbout = async () => {
+//   const res = await fetch('http://localhost:3000/api/about', {
+//       method: 'GET',
+//       headers: {
+//           'Content-Type': 'application/json'
+//       },
+//   })
+//   const data = await res.json();
+//   return data;
+// }
+export default Home
--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,9 +1,11 @@
 "use client"
-import { useRouter } from "next/navigation"
+import { useState } from "react"
 
 
 
-const SimpleButtons = ({ name, url }) => {
+
+
+export const SimpleButtons = ({ name, url }) => {
     const router = useRouter()
     return (
         <div className="hidden w-full md:block md:w-auto">
@@ -18,7 +20,69 @@ const SimpleButtons = ({ name, url }) => {
     )
 }
 
+export const ModalButtons =  ({data, name, url}) => {
+    const users = data.Desarrolladores
+    const [showModal, setShowModal] = useState(false);
+    return (
+        <>
+            <button
+                className="bg-auto-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                type="button"
+                onClick={() => setShowModal(true)}
+            >
+                {name}
+            </button>
+            {showModal ? (
+                <>
+                    <div
+                        className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+                    >
+                        <div className="relative w-auto my-6 mx-auto max-w-3xl">
+                            {/*content*/}
+                            <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                                {/*header*/}
+                                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                                    <h3 className="text-3xl font-semibold">
+                                        Desarrolladores
+                                    </h3>
+                                    <button
+                                        className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                                        onClick={() => setShowModal(false)}
+                                    >
+                                        <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                                            ×
+                                        </span>
+                                    </button>
+                                </div>
+                                {/*body*/}
+                                <div className="relative p-6 flex-auto">
+                                    {users.map((user, index) => (
+                                        <p className="my-4 text-slate-500 text-lg leading-relaxed">
+                                            Name: {user.nombre}  -  Cualidad Registrada: {user.cualidad}
+                                        </p>
+                                    ))}
 
-export default SimpleButtons
 
+                                </div>
+                                {/*footer*/}
+                                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                                    <button
+                                        className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                        type="button"
+                                        onClick={() => {
+                                            setShowModal(false)
+                                        }}
+                                    >
+                                        Close
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+                </>
+            ) : null}
+        </>
+    )
+}
 
