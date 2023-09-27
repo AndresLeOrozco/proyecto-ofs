85711d4ec7ce5dabfbe364ac5c80ae730d1bce2c
Kairo Chacón <49288879+kaisenberg01@users.noreply.github.com>
2023-09-26 22:54:34 -0600

    (Dev): Added useReducer miedo!!
    con UseReducer

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -10,218 +10,195 @@ Component Main page of the project, this is the components that contains all the
 web application. 
 */
 
-"use client"
-import React, { useReducer } from "react"
-import { TextArea } from "@/components/TextArea"
-import {
-  Button,
-} from "@/components/Buttons"
-import { useState, useEffect } from "react"
-import Image from "next/image"
-import play from '../../public/images/play.png'
-import evaluate from '../../public/images/evaluation.png'
-import clear from '../../public/images/clear.png'
-import { Post } from "@/app/RequestFunctions/Post"
-import save from '../../public/images/save.png'
-import edit from '../../public/images/edit.png'
-import upload from '../../public/images/upload.png'
-import { Get } from "@/app/RequestFunctions/Get"
-import { ComboBox } from "@/components/ComboBox"
-import { InputFile } from "@/components/InputFile"
-import Footer from "@/components/Footer"
-import { Alert } from "@/components/Alert"
+"use client";
+import React, { useReducer } from "react";
+import { TextArea } from "@/components/TextArea";
+import { Button } from "@/components/Buttons";
+import { useState, useEffect } from "react";
+import Image from "next/image";
+import play from "../../public/images/play.png";
+import evaluate from "../../public/images/evaluation.png";
+import clear from "../../public/images/clear.png";
+import { Post } from "@/app/RequestFunctions/Post";
+import save from "../../public/images/save.png";
+import edit from "../../public/images/edit.png";
+import upload from "../../public/images/upload.png";
+import { Get } from "@/app/RequestFunctions/Get";
+import { ComboBox } from "@/components/ComboBox";
+import { InputFile } from "@/components/InputFile";
+import Footer from "@/components/Footer";
+import { Alert } from "@/components/Alert";
+
+const regex = /\w+/g;
+
+const initialState = {
+  textEA: "",
+  textTA: "",
+  textRA: "",
+  textLine: 1,
+  inputText: "",
+  allScripts: [],
+  onSelected: false,
+  column: 0,
+  alertText: "",
+  openAlert: false,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "setTextEA":
+      return { ...state, textEA: action.payload };
+    case "setTextTA":
+      return { ...state, textTA: action.payload };
+    case "setTextRA":
+      return { ...state, textRA: action.payload };
+    case "setTextLine":
+      return { ...state, textLine: action.payload };
+    case "setInputText":
+      return { ...state, inputText: action.payload };
+    case "setScripts":
+      return { ...state, allScripts: action.payload };
+    case "setOnSelected":
+      return { ...state, onSelected: action.payload };
+    case "setColumn":
+      return { ...state, column: action.payload };
+    case "setAlertText":
+      return { ...state, alertText: action.payload };
+    case "setOpenAlert":
+      return { ...state, openAlert: action.payload };
+    default:
+      return state;
+  }
+};
 
 const Home = () => {
-  /*
-    react hook of use state, the state is an array that contains
-    The value of the Edition textual Area,the value of the Transpilation Area and
-    The value of the Execution output
-  */
-
-  const [textEA, setTextEA] = useState("")
-  const [textTA, setTextTA] = useState("")
-  const [textRA, setTextRA] = useState("")
-
-  /*
-    react hook of use state. This state is to handle hte posisition
-    of the textarea line. 
-  */
-
-  const [textLine, setTextLine] = useState(1)
-
-  const [inputText, setInputText] = useState("")
-
-  const [allScripts, setScripts] = useState([])
-
-  const [onSelected, setOnSelected] = useState(false)
-
-  const [column, setColumn] = useState(0)
-
-  const [alertText, setAlertText] = useState("")
-
-  const [openAlert, setOpenAlert] = useState(false)
-   
-
-
-
-  /*
-     Function that changes the The value of the Edition textual Area
-  */
-
-
-
-  /*
-    Function that changes the The value of the Transpilation Area
-  */
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const handleTranspileClick = async () => {
-    const compiledText = await Post({ text: textEA }, 'compile')
-    const NewText = `${compiledText.time}\n${compiledText.text}`
-    setTextTA(NewText)
-  }
+    const compiledText = await Post({ text: state.textEA }, "compile");
+    const NewText = `${compiledText.time}\n${compiledText.text}`;
+    dispatch({ type: "setTextTA", payload: NewText });
+  };
 
-  /*
-    Function that changes the The value of the Terminal
-  */
   const handleEvalClick = async () => {
-    const terminalText = await Post({ text: "Siuuuuu.txt" }, "eval")
-    setTextRA(terminalText)
-  }
-
-
-  const regex = /\w+/g
-
-  /*
-    Function that set/clear all textareas after click on the ClickButton.
-  */
+    const terminalText = await Post({ text: "Siuuuuu.txt" }, "eval");
+    dispatch({ type: "setTextRA", payload: terminalText });
+  };
 
   const handleClearClick = () => {
-    setTextEA("")
-    setTextTA("")
-    setTextRA("")
-    setInputText("")
-    setScripts([])
-    handleRecoverScript()
-  }
+    dispatch({ type: "setTextEA", payload: "" });
+    dispatch({ type: "setTextTA", payload: "" });
+    dispatch({ type: "setTextRA", payload: "" });
+    dispatch({ type: "setInputText", payload: "" });
+    dispatch({ type: "setScripts", payload: [] });
+    handleRecoverScript();
+  };
 
   const setAndShowAlert = (message) => {
-    setAlertText(message)
-    setOpenAlert(true)
-  }
+    dispatch({ type: "setAlertText", payload: message });
+    dispatch({ type: "setOpenAlert", payload: true });
+  };
 
+  const handleCursorLine = (line) => {
+    dispatch({ type: "setTextLine", payload: line });
+  };
 
-  const handleCursorLine = line => setTextLine(line)
-
-
-  /*
-    This is to recover all the scrips (just name).
-   */
   const handleRecoverScript = async () => {
-    const scripts = await Get('script')
-    setScripts(scripts)
-  }
-
+    const scripts = await Get("script");
+    dispatch({ type: "setScripts", payload: scripts });
+  };
 
   const handleSelectFile = async (selected) => {
     const file = selected ? await Get(`script/${selected}`) : selected;
-    setTextEA(file)
-    handleOnSelected(file)
-  }
+    dispatch({ type: "setTextEA", payload: file });
+    handleOnSelected(file);
+  };
 
-  const handleInputText = file => setInputText(file)
+  const handleInputText = (file) => {
+    dispatch({ type: "setInputText", payload: file });
+  };
 
   const handleSaveClick = async () => {
-    let message = "Error, Empty EA Area or FileName field"
-    if (textEA && inputText) {
-      const nameFile = await Post(textEA, `script/${inputText}`)
-      handleRecoverScript()
-      handleOnSelected(inputText)
-      handleSelectFile(inputText)
-      message = "Succesfully Saved"
+    let message = "Error, Empty EA Area or FileName field";
+    if (state.textEA && state.inputText) {
+      const nameFile = await Post(state.textEA, `script/${state.inputText}`);
+      handleRecoverScript();
+      handleOnSelected(state.inputText);
+      handleSelectFile(state.inputText);
+      message = "Succesfully Saved";
     }
 
-    setAndShowAlert(message)
+    setAndShowAlert(message);
+  };
 
-  }
+  const handleOnSelected = (text) => {
+    text
+      ? dispatch({ type: "setOnSelected", payload: true })
+      : dispatch({ type: "setOnSelected", payload: false });
+  };
 
-  const handleOnSelected = text => text ? setOnSelected(true) : setOnSelected(false)
+  const handleEditClick = () => {
+    dispatch({ type: "setOnSelected", payload: false });
+  };
 
-  const handleEditClick = () => setOnSelected(false)
-
-  /*
-    This useEffect is just to load all the information
-    related to the scripts after either reload the page
-    or the first time loading.
-  */
   useEffect(() => {
-    handleRecoverScript()
-  }, [])
+    handleRecoverScript();
+  }, []);
 
-
-  /*
-    Component that contains the REACT (JSX) code of the body the app
-  */
   return (
     <main>
-      <ComboBox selectedFile={handleSelectFile} items={allScripts} updateInputText={handleInputText} />
-      <InputFile onOff={onSelected} selectedFile={inputText} updateInputText={handleInputText} />
+      <ComboBox
+        selectedFile={handleSelectFile}
+        items={state.allScripts}
+        updateInputText={handleInputText}
+      />
+      <InputFile
+        onOff={state.onSelected}
+        selectedFile={state.inputText}
+        updateInputText={handleInputText}
+      />
       <div className="text-all">
         <div className="text-EA">
           <TextArea
             Area="OFS"
-            GetText={setTextEA}
-            AreaText={textEA}
+            GetText={(text) => dispatch({ type: "setTextEA", payload: text })}
+            AreaText={state.textEA}
             GetLine={handleCursorLine}
-            Column={setColumn}
+            Column={(col) => dispatch({ type: "setColumn", payload: col })}
           />
           <div className="btns-all">
-            <Button
-              clickEvent={handleTranspileClick}
-              title="Compile"
-            >
+            <Button clickEvent={handleTranspileClick} title="Compile">
               <Image
                 src={play}
-                alt='This is a play button img'
+                alt="This is a play button img"
                 className="img-play"
               />
             </Button>
-            <Button
-              clickEvent={handleEvalClick}
-              title="Evaluate"
-            >
+            <Button clickEvent={handleEvalClick} title="Evaluate">
               <Image
                 src={evaluate}
-                alt='This is a evaluate button img'
+                alt="This is a evaluate button img"
                 className="img-play"
               />
             </Button>
-            <Button
-              clickEvent={handleSaveClick}
-              title={"Save"}
-            >
+            <Button clickEvent={handleSaveClick} title={"Save"}>
               <Image
                 src={save}
-                alt='This is a save button img'
+                alt="This is a save button img"
                 className="img-save"
               />
             </Button>
-            <Button
-              clickEvent={handleEditClick}
-              title={"Edit"}
-            >
+            <Button clickEvent={handleEditClick} title={"Edit"}>
               <Image
                 src={edit}
-                alt='This is a edit button img'
+                alt="This is a edit button img"
                 className="img-edit"
               />
             </Button>
-            <Button
-              clickEvent={handleClearClick}
-              title={"Clear"}
-            >
+            <Button clickEvent={handleClearClick} title={"Clear"}>
               <Image
                 src={clear}
-                alt='This is a clear button img'
+                alt="This is a clear button img"
                 className="img-clear"
               />
             </Button>
@@ -230,7 +207,7 @@ const Home = () => {
         <div className="text-TA">
           <TextArea
             Area="JS"
-            AreaText={textTA}
+            AreaText={state.textTA}
             NotEditable="pointer-events-none"
           />
         </div>
@@ -238,18 +215,26 @@ const Home = () => {
         <div className="text-RA">
           <TextArea
             Area="Terminal"
-            AreaText={textRA}
+            AreaText={state.textRA}
             NotEditable="pointer-events-none"
           />
         </div>
       </div>
-      <Alert text={alertText} open={openAlert} setOpen = {setOpenAlert} />
-      <Footer information={[`Words: ${textEA.match(regex)?.length || 0}`, `Line: ${textLine}`, `Row: ${textEA.split("\n").length}`, `Col: ${column}`]} />
-    </main >
-  )
-}
+      <Alert
+        text={state.alertText}
+        open={state.openAlert}
+        setOpen={(open) => dispatch({ type: "setOpenAlert", payload: open })}
+      />
+      <Footer
+        information={[
+          `Words: ${state.textEA.match(regex)?.length || 0}`,
+          `Line: ${state.textLine}`,
+          `Row: ${state.textEA.split("\n").length}`,
+          `Col: ${state.column}`,
+        ]}
+      />
+    </main>
+  );
+};
 
-
-
-
-export default Home
+export default Home;
