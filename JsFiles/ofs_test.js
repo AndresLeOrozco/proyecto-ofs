533de570e1dd5b367158a50d936541547d2eae16
eduardoojeda <peleojeda@gmail.com>
2023-10-25 02:04:18 -0600

    SIUUUU

--- JsFiles/ofs_test.js
+++ JsFiles/ofs_test.js
@@ -1,12 +1,12 @@
-// import { Stream, iterate } from "../src/utils/stream.js";
+import { Stream, iterate } from "../src/utils/stream.js";
 
-// const nats = iterate(0, x => x + 1)
-// const even = new Stream(nats).filter(x => x % 2 == 0);
-// const evenLessThanEleven = even.filter(x => x<11);
-// evenLessThanEleven.map(x => console.log(x)).toList();
+const nats = iterate(0, x => x + 1)
+const even = new Stream(nats).filter(x => x % 2 == 0);
+const evenLessThanEleven = even.filter(x => x<11);
+evenLessThanEleven.map(x => console.log(x)).toList();
 
 
-console.log("Hola mundo!")
+// console.log("Hola mundo!")
 
 
 
--- src/data/eval/CRUD.js
+++ src/data/eval/CRUD.js
@@ -11,31 +11,19 @@ this file recover a fs file and returns its content
 
 import fs from "fs/promises"
 import path from "path"
-import {exec} from "child_process"
+import { promisify } from 'util';
+import { exec } from 'child_process';
+
+const execPromise = promisify(exec);
 
 export const EvaluateFile = async (name) => {
 
-    const filePath = path.join(process.cwd(), "jsFiles", name)
-    const {stdout} = exec('node jsFiles/ofs_test.js', (error, stdout, stderr) => {
-        if (error) {
-          console.error(`error: ${error.message}`);
-          return;
-        }
-      
-        if (stderr) {
-          console.error(`stderr: ${stderr}`);
-          return;
-        }
-      
-        console.log(`stdout:\n${stdout}`);
-        const a = stdout;
-        return stdout
-      });
-      
-
-    console.log("Onichan ",stdout)
+    
+    
     try {
-        const fileContent = await fs.readFile(filePath, "utf-8")
+        const filePath = path.join(process.cwd(), "jsFiles", name)
+        const {stdout, stderr} = await execPromise(`node ${filePath}`)
+        console.error(stderr)
         return stdout;
     } catch (err) {
         throw("Error reading file: ", name);
